ce3f79538e9b24382f2f2aede0488374c226933d
rjkram <rjk5079@gmail.com>
2026-07-28 19:40:39 +0000

    Deploying to gh-pages from @ rfmip/rfmip.github.io@92f6bfd228eadfe057342d42c9fa985f46f7d145 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -413,10 +413,12 @@ ninja.data = [{
           description: "",
           section: "News",handler: () => {
               window.location.href = "/news/announcement_3/";
-            },},{id: "news-final-version-of-the-rfmip2-0-protocol-paper-has-now-been-published-in-geoscientific-model-development-gmd-https-gmd-copernicus-org-articles-19-4447-2026",
-          title: 'Final version of the RFMIP2.0 Protocol Paper has now been published in Geoscientific...',
+            },},{id: "news-rfmip2-0-protocol-published-in-gmd",
+          title: 'RFMIP2.0 Protocol Published in GMD',
           description: "",
-          section: "News",},{
+          section: "News",handler: () => {
+              window.location.href = "/news/announcement_4/";
+            },},{
         id: 'social-email',
         title: 'email',
         section: 'Socials',
